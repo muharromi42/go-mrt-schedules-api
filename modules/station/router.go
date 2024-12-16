119723277b3f00ecba84ddd8243fcd8eb3651881
muharromi42 <muharromi42@gmail.com>
2024-12-16 21:36:22 +0700

    router checkschedulesbystation

--- modules/station/router.go
+++ modules/station/router.go
@@ -16,6 +16,10 @@ func Initiate(router *gin.RouterGroup) {
 		// code service
 		GetAllStation(c, stationService)
 	})
+
+	station.GET("", func(c *gin.Context){
+		CheckSchedulesByStation(c, stationService)
+	})
 }
 
 func GetAllStation(c *gin.Context, service Service)  {
@@ -38,6 +42,32 @@ func GetAllStation(c *gin.Context, service Service)  {
 		response.APIResponse{
 			Success: true,
 			Message: "Successfully get all station",
+			Data: datas,
+		},
+	)
+}
+
+
+func CheckSchedulesByStation(c *gin.Context, service Service) {
+	id := c.Param("id")
+
+	datas, err := service.CheckSchedulesByStation(id)
+	if err != nil {
+		// handle error
+		c.JSON(http.StatusBadRequest, 
+			response.APIResponse{
+				Success: false,
+				Message: err.Error(),
+				Data: nil,
+			},
+		)
+		return
+	}
+	c.JSON(
+		http.StatusOK,
+		response.APIResponse{
+			Success: true,
+			Message: "Successfully get schedules by station",
 			Data: datas,
 		},
 	)

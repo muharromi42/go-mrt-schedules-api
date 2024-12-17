80d6929ac0282de42a1f670d81706f6232c6cbe5
muharromi42 <muharromi42@gmail.com>
2024-12-17 23:24:56 +0700

    dto schedules

--- modules/station/dto.go
+++ modules/station/dto.go
@@ -9,3 +9,15 @@ type StationResponse struct {
 	Id   string `json:"nid"`
 	Name string `json:"title"`
 }
+
+type Schedule struct {
+	StationId          string `json:"mid"`
+	StationName        string `json:"title"`
+	ScheduleBundaranHI string `json:"jadwal_hi_biasa"`
+	ScheduleLebakBulus string `json:"jadwal_lb_biasa"`
+}
+
+type ScheduleResponse struct {
+	StationName string `json:"station"`
+	Time        string `json:"time"`
+}
